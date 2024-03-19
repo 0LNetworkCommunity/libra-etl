dda93b85a536232dd8f9c479130d8dc4e43da94d
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2024-03-19 17:27:58 +0000

    scaffold tx split to vertex and edge

--- src/version_five_tx_log.rs
+++ src/version_five_tx_log.rs
@@ -5,17 +5,38 @@ use std::path::Path;
 
 use diem_json_rpc::views::TransactionView;
 
-fn load_tx_json(path: &Path) -> anyhow::Result<()> {
+pub fn load_tx_json_from_file(path: &Path) -> anyhow::Result<Vec<TransactionView>> {
     let json_string = std::fs::read_to_string(path)?;
-    let txs: Vec<TransactionView> = serde_json::from_str(&json_string)?;
-    dbg!(&txs);
-    Ok(())
+    Ok(serde_json::from_str(&json_string)?)
+}
+
+pub fn split_tx_vertex_edge(tx_view: &TransactionView) -> anyhow::Result<()> {
+  match &tx_view.transaction {
+    diem_json_rpc::views::TransactionDataView::UserTransaction { sender, script, .. } => {
+
+      // TODO: parse the script
+      dbg!(&sender);
+      dbg!(&script);
+
+      // TODO: make a vertex
+
+      // TODO: make an edge
+
+    },
+    _ => {
+      println!("{:#?}", &tx_view.transaction);
+      println!("...not a user tx");
+    },
+  };
+
+  Ok(())
 }
 
 pub(crate) fn debug_parse_manifest() {
     let this_path = Path::new(env!("CARGO_MANIFEST_DIR"));
     let this_path = this_path.join("fixtures/v5_sample_tx_log.json");
-    load_tx_json(&this_path).expect("parse json");
+    let tx_vec = load_tx_json_from_file(&this_path).expect("parse json");
+    split_tx_vertex_edge(&tx_vec.get(0).unwrap()).unwrap();
 }
 
 #[test]
